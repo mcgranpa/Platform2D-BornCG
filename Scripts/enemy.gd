85aa5b52080959f3c0c4049f2d3ef39ba2ee45c2
mcgranpa <fmcmeans@gmail.com>
2020-10-30 16:50:48 -0500

    part13 Final
    completed part13 - creating emenies and ray casting.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,22 +2,38 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 export var direction = -1
+export var detects_cliff = true
 
 const SPEED = 50
 const GRAVITY = 20
 
 
 func _ready():
-	if direction == 1:
-		$AnimatedSprite.flip_h = true
+	flip()
 	
 func _physics_process(delta):
-	if  is_on_wall():
+	
+	## if enemy does not detect a floor under the ray cast 
+	## then the enemy is at the edge of the platform and 
+	## would fall off if it kept going.  But the deteect cliff
+	## option must also be true
+	var detect_drop_off = not $floor_checker.is_colliding() and detects_cliff
+	
+	## if enemy is collioding with a wall or other object or it has 
+	## detect a cliff then reverse.  is-on_floor prevents enemy from 
+	## flipping if he's falling.  
+	if  is_on_wall() or (detect_drop_off and is_on_floor()):
 		direction *= -1
-		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+		flip()
 	
 	velocity.y += GRAVITY
 	
 	velocity.x = SPEED * direction 
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
+func flip():
+	if direction == 1:
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.flip_h = false
+	$floor_checker.position.x = $CollisionShape2D.shape.get_extents().x * direction
